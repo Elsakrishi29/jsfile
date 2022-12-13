da8da396e0db3821a60aa990c49c3dc8e1269d4c
Krishnaveni <veniumabala@gmail.com>
2022-12-13 13:40:56 +0000

    conditions task

---
+++ condition/main.js
@@ -0,0 +1,26 @@
+
+
+
+
+const num=prompt("Enter the number");
+if(num>0){
+     document.write(num+ " is +ve number ");
+}
+else if(num<0){
+    document.write(num+ " is -ve number ");
+}
+else{
+    document.write("This num is zero");
+}
+
+
+                            //comparison operator
+
+var a=10;
+var b=10;
+
+var c=a===b;
+console.log(c);
+
+                        //switch 
+
